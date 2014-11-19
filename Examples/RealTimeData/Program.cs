1b60f9fe14970d75fa4163513066d6e9dd702cbd
Pavel Koneski <pavel.koneski@gmail.com>
2014-11-18 19:59:03 -0800

    Initial projects created.

---
+++ Examples/RealTimeData/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
